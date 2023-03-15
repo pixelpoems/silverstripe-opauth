2d17c9b9a5531148efdbd806970be7d007d4b78d
Bettina Bröthaler <bettina.broethaler@gmx.at>
2023-03-15 14:44:09 +0100

    Updated OpauthIdentity class within OpauthMemberExtension

--- src/Extensions/OpauthMemberExtension.php
+++ src/Extensions/OpauthMemberExtension.php
@@ -2,18 +2,17 @@
 namespace Silverstripe\Opauth\Extensions;
 
 
+use Silverstripe\Opauth\Models\OpauthIdentity;
 use SilverStripe\ORM\DataExtension;
 
 class OpauthMemberExtension extends DataExtension
 {
-
     private static $has_many = array(
-        "OpauthIdentities" => "OpauthIdentity"
+        "OpauthIdentities" => OpauthIdentity::class
     );
 
     public function onBeforeDelete()
     {
         $this->owner->OpauthIdentities()->removeAll();
     }
-
 }
